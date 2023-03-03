6e9ee09979f9713f57f58328c12f36cc6ca64893
Juan Pablo Saluzzo <saluzzojuampi@gmail.com>
2023-03-03 16:37:29 -0300

    login process nuevo implementado

--- database/config/config.js
+++ database/config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "49800jil02",
     "database": "courses",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "49800jil02",
     "database": "courses",
     "host": "127.0.0.1",
     "dialect": "mysql"
